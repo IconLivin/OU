095754aa93562b019d48ce61d6d6af123aecde10
IconLivin <anton.parole@mail.ru>
2019-10-18 09:26:22 +0300

    ok

--- src/noise.cpp
+++ src/noise.cpp
@@ -128,7 +128,7 @@ Mat createNoise(Mat image, Mat noise)
 
 
 
-Mat Generate_Mask_Gauss(int size_x, int size_y,double pr=0.5) {
+Mat Generate_Mask_Gauss(int size_x, int size_y,double pr=0.3) {
 	random_device rd;
 	mt19937 gen(rd());
 	normal_distribution<> d(20, 1);
@@ -147,10 +147,10 @@ Mat Generate_Mask_Gauss(int size_x, int size_y,double pr=0.5) {
 	return result;
 }
 
-Mat Generate_Mask_Gamma(int size_x, int size_y,double pr=0.5) {
+Mat Generate_Mask_Gamma(int size_x, int size_y,double pr=0.3) {
 	random_device rd;
 	mt19937 gen(rd());
-	gamma_distribution<> d(10,1);
+	gamma_distribution<> d(9, 0.5);
 	Mat result(size_x, size_y, CV_8UC3);
 	srand(time(NULL));
 	for (int i = 0; i < result.rows; i++) {
@@ -185,7 +185,7 @@ float** Gauss_Kernel(int radius, float sigma) {
 	return kernel;
 }
 
-Vec3b Get_Color(float** kernel, Mat img, int size, int x, int y) {
+Vec3b Get_Color_Gauss(float** kernel, Mat img, int size, int x, int y) {
 	Vec3b color = 0;
 	int x1 = 0, y1 = 0;
 	for (int i = x; i < size + x; i++) {
@@ -207,7 +207,7 @@ Mat Gauss_Filter(Mat img) {
 	float** kernel = Gauss_Kernel(n, 2);
 	for (int i = 2 * n + 1; i < img.rows - (2 * n + 1); i++) {
 		for (int j = 2 * n + 1; j < img.cols - (2 * n + 1); j++) {
-			result.at<Vec3b>(i, j) = Get_Color(kernel, img, 2 * n + 1, i, j);
+			result.at<Vec3b>(i, j) = Get_Color_Gauss(kernel, img, 2 * n + 1, i, j);
 		}
 	}
 	return result;
@@ -217,89 +217,40 @@ int median(vector <int> v)
 {
 	
 	sort(v.begin(), v.end(), greater<int>());
-	return v[4];
+	return v[v.size()/2];
 
 }
+
+Vec3b Get_Color_Median(Mat img, int size, int x, int y) {
+	Vec3b color = 0;
+	int x1 = 0, y1 = 0;
+	vector <int > b, g, r;
+	for (int i = x; i < size + x; i++) {
+		y1 = 0;
+		for (int j = y; j < size + y; j++) {
+			b.push_back((int)img.at <Vec3b>(i, j)[0]);
+			g.push_back((int)img.at <Vec3b>(i, j)[1]);
+			r.push_back((int)img.at <Vec3b>(i, j)[2]);
+			y1++;
+		}
+		x1++;
+	}
+	color[0] = median(b);
+	color[1] = median(g);
+	color[2] = median(r);
+	return color;
+}
+
 Mat median_filter(Mat img)
 {
 	const int n = 3;
-	Mat copy(img.rows + 2, img.cols + 2, CV_8UC3);
-	Mat result(img.rows, img.cols, CV_8UC3);
-	cout << img.cols << endl;
-	for (int i = 0; i < copy.rows; i++)
-		for (int j = 0; j < copy.cols; j++)
+	Mat result=img.clone();
+	for (int i = 0; i < result.rows - n; i++) {
+		for (int j = 0; j < result.cols - n; j++)
 		{
-			copy.at<Vec3b>(i, j)[0] = 0;
-			copy.at<Vec3b>(i, j)[1] = 0;
-			copy.at<Vec3b>(i, j)[2] = 0;
-		}
-	for (int i = 0; i < 3; i++) {
-		copy.at<Vec3b>(0, 0)[i] = img.at <Vec3b>(0, 0)[i];
-		copy.at<Vec3b>(copy.rows - 1, 0)[i] = img.at <Vec3b>(img.rows - 1, 0)[i];
-		copy.at<Vec3b>(0, copy.cols - 1)[i] = img.at <Vec3b>(0, img.cols - 1)[i];
-		copy.at<Vec3b>(copy.rows - 1, copy.cols - 1)[i] = img.at <Vec3b>(img.rows - 1, img.cols - 1)[i];
-	}
-	for (int i = 0; i < copy.rows; i++) {
-		for (int j = 0; j < copy.cols; j++)
-		{
-			if (j > 0 && i > 0 && j < copy.cols - 1 && i < copy.rows - 1)
-			{
-				copy.at<Vec3b>(i, j)[0] = img.at <Vec3b>(i - 1, j - 1)[0];
-				copy.at<Vec3b>(i, j)[1] = img.at <Vec3b>(i - 1, j - 1)[1];
-				copy.at<Vec3b>(i, j)[2] = img.at <Vec3b>(i - 1, j - 1)[2];
-			}
-			else if (i == 0 && j > 0 && j < copy.cols - 1) {
-				copy.at<Vec3b>(i, j)[0] = (int)img.at <Vec3b>(i, j - 1)[0];
-				copy.at<Vec3b>(i, j)[1] =(int) img.at <Vec3b>(i, j - 1)[1];
-				copy.at<Vec3b>(i, j)[2] = (int)img.at <Vec3b>(i, j - 1)[2];
-			}
-			else if (i == copy.rows - 1 && j > 0 && j < copy.cols)
-			{
-				copy.at<Vec3b>(i, j)[0] = (int)copy.at <Vec3b>(i - 1, j)[0];
-				copy.at<Vec3b>(i, j)[1] = (int)copy.at <Vec3b>(i - 1, j)[1];
-				copy.at<Vec3b>(i, j)[2] = (int)copy.at <Vec3b>(i - 1, j)[2];
-			}
-			else if (j == copy.cols - 1 && i > 0 && i < copy.rows - 1)
-			{
-				copy.at<Vec3b>(i, j)[0] = copy.at<Vec3b>(i, j - 1)[0];
-				copy.at<Vec3b>(i, j)[1] = copy.at<Vec3b>(i, j - 1)[1];
-				copy.at<Vec3b>(i, j)[2] = copy.at<Vec3b>(i, j - 1)[2];
-			}
-			else if (j == 0 && i > 0 && i < copy.rows - 1)
-			{
-				copy.at<Vec3b>(i, j)[0] = img.at<Vec3b>(i - 1, j)[0];
-				copy.at<Vec3b>(i, j)[1] = img.at<Vec3b>(i - 1, j)[1];
-				copy.at<Vec3b>(i, j)[2] = img.at<Vec3b>(i - 1, j)[2];
-			}
-			/*namedWindow("cpy1", WINDOW_NORMAL);
-			imshow("cpy1", copy);
-			waitKey();*/
+			result.at<Vec3b>(i, j) = Get_Color_Median(result, n, i, j);
 		}
 	}
-	destroyWindow("cpy");
-	for (int i = 1; i < copy.rows - 1; i++){
-		for (int j = 1; j < copy.cols - 1; j++)
-		{
-			vector <int > b, g, r;
-			for (int x = -1; x < 2; x++)
-				for (int y = -1; y < 2; y++) {
-					b.push_back((int)copy.at <Vec3b>(i + x, j + y)[0]);
-					g.push_back((int)copy.at <Vec3b>(i + x, j + y)[1]);
-					r.push_back((int)copy.at <Vec3b>(i + x, j + y)[2]);
-				}
-			result.at<Vec3b>(i - 1, j - 1)[0] = median(b);
-			result.at<Vec3b>(i - 1, j - 1)[1] = median(g);
-			result.at<Vec3b>(i - 1, j - 1)[2] = median(r);
-			b.clear();
-			g.clear();
-			r.clear(); 
-			r.clear(); 
-		}
-}
-	namedWindow("cpy1", WINDOW_NORMAL);
-	imshow("cpy1", result);
-	waitKey();
-	
 	return result;
 }
 
